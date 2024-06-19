b8a526427366a49bd8e82e4a0846b0619dfa283e
Tushar Pawar <tushharpawar14@gmail.com>
2024-06-19 20:37:18 +0530

    created post controller

---
+++ src/app/api/v1/post-upload/route.ts
@@ -0,0 +1,45 @@
+import dbConnect from "@/lib/dbConnect";
+import PostModel from "@/models/Post.model";
+import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_TOKEN,
+});
+
+export const POST = async (req: NextRequest, res: NextResponse) =>{
+    dbConnect()
+
+    try {
+            const formData = await req.formData()
+            const postUrl = formData.get('postUrl') as string
+            const caption = formData.get('caption') as string
+
+            const cloudinaryUrl = await cloudinary.uploader.upload(postUrl,{
+                folder:"user-posts"
+            }) 
+
+            const newPost = new PostModel({
+                postUrl:cloudinaryUrl.secure_url,
+                caption,
+            })
+
+            await newPost.save();
+
+            console.log(newPost);
+            
+            return NextResponse.json({
+                success: true,
+                message: "Post uploaded successfully!",
+            }, {status: 201});
+
+    } catch (error) {
+        console.log("Error in posting images: ", error);
+        return NextResponse.json({
+            suceess:false,
+            message: "Failed to post an image"}
+        ,{status: 400});
+    }
+}
