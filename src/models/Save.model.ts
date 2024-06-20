3d247576d24877ec56e7c16298539ce03595b14c
Tushar Pawar <tushharpawar14@gmail.com>
2024-06-20 22:39:46 +0530

    Created Save model

---
+++ src/models/Save.model.ts
@@ -0,0 +1,26 @@
+import mongoose,{Schema,Document} from "mongoose";
+
+export interface Save extends Document{
+    name:string;
+    post:any;
+    owner:any;
+    createdAt:Date;
+    updatedAt:Date;
+}
+
+export const SaveSchema :Schema<Save> =new mongoose.Schema({
+    name:{
+        type:String
+    },
+    post:{
+        type:Schema.Types.ObjectId,
+        ref:"Post"
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+},{timestamps:true})
+
+const SaveModel = mongoose.models.Save as mongoose.Model<Save> || mongoose.model<Save> ("Save",SaveSchema)
+export default SaveModel
