6c632e9abc98e9eb658d82cc68fa73dcdf24ca02
Tushar Pawar <tushharpawar14@gmail.com>
2024-06-20 22:39:31 +0530

    Created like model

---
+++ src/models/Like.model.ts
@@ -0,0 +1,30 @@
+import mongoose ,{Schema,Document} from "mongoose";
+
+export interface Like extends Document {
+    post:any;
+    comment:any;
+    note:any;
+    likedBy:any;
+}
+
+export const LikeSchema : Schema<Like> = new mongoose.Schema({
+    post:{
+        type:Schema.Types.ObjectId,
+        ref:"Post"
+    },
+    comment:{
+        type:Schema.Types.ObjectId,
+        ref:"Comment"
+    },
+    note:{
+        type:Schema.Types.ObjectId,
+        ref:"Note"
+    },
+    likedBy:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+    }
+},{timestamps:true})
+
+const LikeModel = mongoose.models.Like as mongoose.Model<Like> || mongoose.model<Like>("Like",LikeSchema)
+export default LikeModel;
