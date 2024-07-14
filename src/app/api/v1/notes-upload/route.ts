19df764dd2209164747d2bbb33db8d828b471c1a
Tushar Pawar <tushharpawar14@gmail.com>
2024-07-14 16:07:49 +0530

    Fetch userId from session

--- src/app/api/v1/notes-upload/route.ts
+++ src/app/api/v1/notes-upload/route.ts
@@ -1,11 +1,27 @@
 import dbConnect from "@/lib/dbConnect";
 import NotesModel from "@/models/Note.model";
 import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
 
 export async function POST(req:NextRequest,res:NextResponse) {
         await dbConnect()
+
+        //Getting userId from logged in user from Session
+
+    const session = await getServerSession(authOptions)
+    const _user = session?.user;
+
+    if(!session || !_user){
+        return NextResponse.json(
+            { success: false, message: 'Not authenticated' },
+            { status: 401 }
+          );
+    }
+
+    const userId = _user._id;
         try {
-            const {content,userId} = await req.json();
+            const {content} = await req.json();
 
             const newNote = new NotesModel({
                 content,
