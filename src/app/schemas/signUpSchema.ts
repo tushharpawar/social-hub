e1e68ee5b919dd3a7178f96b9c09ef372b9ca7de
Tushar Pawar <tushharpawar14@gmail.com>
2024-07-20 22:41:16 +0530

    created signUpSchema for zod validation

---
+++ src/app/schemas/signUpSchema.ts
@@ -0,0 +1,14 @@
+import {z} from "zod"
+
+export const signUpSchema = z.object({
+    username:z.string()
+    .min(2,"Usernameshould not be less than 2 characters")
+    .max(15,"Username must be less than 15 characters")
+    .regex(/^[a-z][a-z0-9._]+$/,'Username must not contain special characters'),
+
+    email:z.string().email({message:"Invalid email address"}),
+
+    password:z.string().min(8,{message:"Password should not be less than 8 characters"}),
+
+    fullName:z.string(),
+})
