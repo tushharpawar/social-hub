089b2cee15d1222c7f1fe731dca075d6a6873fb1
Tushar Pawar <tushharpawar14@gmail.com>
2024-07-21 22:41:14 +0530

    created background for home page

---
+++ src/app/aceternity/backGround.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { motion } from "framer-motion";
+import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
+
+export function HeroHighlightDemo() {
+  return (
+    <HeroHighlight>
+      <motion.h1
+        initial={{
+          opacity: 0,
+          y: 20,
+        }}
+        animate={{
+          opacity: 1,
+          y: [20, -5, 0],
+        }}
+        transition={{
+          duration: 0.5,
+          ease: [0.4, 0.0, 0.2, 1],
+        }}
+        className="text-2xl mt-19 px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+      >
+        Welcome to <Highlight>Social-Hub</Highlight>.
+        <p> Let&apos;s get connected to World.</p>
+      </motion.h1>
+    </HeroHighlight>
+  );
+}
