2fa5c1cb35b38003b79e2d84a26b48eccd782706
Tushar Pawar <tushharpawar14@gmail.com>
2024-07-22 22:50:14 +0530

    Updated app page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { HeroHighlight } from "@/components/ui/hero-highlight";
+import { HomePage } from "./aceternity/HomePage";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center">
-      
+    <main className=" w-full min-h-screen">
+      <HomePage></HomePage>
     </main>
   );
 }
