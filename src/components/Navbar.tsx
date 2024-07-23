b5005ce72ad3cdf6558e3271ade73000f3ba6547
Tushar Pawar <tushharpawar14@gmail.com>
2024-07-23 23:49:18 +0530

    Updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[10vh] mt-5 flex justify-between items-center">
-          <div className=" font-bold text-2xl mx-28 ">Social-Hub</div>
-          <div className=" flex gap-7 font-bold mx-28">
-            <div ><Button  className=" text-md ">Sign up</Button></div>
-            <div ><Button variant="outline" className=" text-md ">Login</Button></div>
+    <div className="w-full h-[10vh] mt-5 flex gap-[40rem] justify-center items-center">
+          <div className=" sm:text-md font-bold text-2xl ">Social-Hub</div>
+          <div className=" flex gap-5 font-bold ">
+            <div ><Button  className=" text-md ">Log in</Button></div>
+            <div ><Button variant="outline" className=" text-md ">Sign up</Button></div>
           </div>
         </div>
   )
