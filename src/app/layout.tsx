a8cb054842afc28cc1e255d95d6eeff20a0bbbfd
Tushar Pawar <tushharpawar14@gmail.com>
2024-07-24 11:27:38 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from '../context/AuthProvider'
+import { Toaster } from "@/components/ui/toaster"
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <main>{children}</main>
+      <Toaster />
+      </body>
       </AuthProvider>
     </html>
   );
