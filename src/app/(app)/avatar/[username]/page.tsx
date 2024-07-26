2ca9ed13b806bd17a75818416968d79e9fa8f19d
Tushar Pawar <tushharpawar14@gmail.com>
2024-07-26 23:22:54 +0530

    Added an dialog box when image is selected

--- src/app/(app)/avatar/[username]/page.tsx
+++ src/app/(app)/avatar/[username]/page.tsx
@@ -1,133 +1,149 @@
-'use client'
-import React, { useState } from 'react'
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import Image from 'next/image'
-import { useParams } from 'next/navigation'
-import 'react-image-crop/dist/ReactCrop.css'
-import ReactCrop, { type Crop } from 'react-image-crop'
-
+/* eslint-disable react/jsx-no-comment-textnodes */
+"use client";
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import "react-image-crop/dist/ReactCrop.css";
+import ReactCrop, { type Crop } from "react-image-crop";
+import { Button } from "@/components/ui/button";
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog"
-  import { Button } from "@/components/ui/button"
-  import makeAspectCrop from "../../../../../node_modules/react-image-crop/dist/utils"
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
-const ASPECT_RETIO = 1
-const MIN_DIMENTION = 150
+const ASPECT_RETIO = 1;
+const MIN_DIMENTION = 150;
+
 const Page = () => {
+  const [imgSrc, setImgSrc] = useState("");
 
-    const [imgSrc,setImgSrc] = useState("")
+  const [crop, setCrop] = useState<Crop>();
 
-    const [crop,setCrop] = useState<Crop>()
-    const [open,setOpen] = useState(false)
+  const params = useParams<{ username: string }>();
 
-    const params = useParams<{username:string}>()
+  const onSelectFile = (e: any) => {
+    const file = e.target.files?.[0];
 
-    const onSelectFile = (e : any) =>{
-        const file = e.target.files?.[0]
+    if (!file) return;
 
-        if(!file) return;
+    const reader = new FileReader();
 
-        const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      if(imgSrc) setImgSrc("") 
+      const imgUrl = reader.result?.toString() || "";
+      console.log(imgUrl);
+      setImgSrc(imgUrl);
+    });
 
-        reader.addEventListener("load",()=>{
-            const imgUrl = reader.result?.toString() || "";
-            console.log(imgUrl);
-            setImgSrc(imgUrl);
-            setOpen(true)
-        })
+    reader.readAsDataURL(file);
+  };
 
-        reader.readAsDataURL(file);
+  const onImageLoad = (e: any) => {
 
-    }
+    const width = 50;
+    const height = 50;
+    setCrop({
+      unit: "%",
+      x: 10,
+      y: 10,
+      width,
+      height,
+    });
+  };
 
-    const onImageLoad = (e : any)=> {
-        const width =75
-        const height= 75
-        setCrop({
-            unit:"%",
-            x:10,
-            y:10,
-            width,
-            height,
-        })
-    }
+  const cancelHandler = () =>{
+        setImgSrc("");
+  }
 
+  const continueHandler = () =>{
+        setImgSrc("");
+  }
   return (
-    <div className='min-h-screen flex justify-center items-center bg-slate-100'>
-        <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+    <div className="min-h-screen flex justify-center items-center bg-slate-100">
+      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
         <div className="text-center">
           <h1 className="text-sm font-extrabold tracking-tight lg:text-4xl">
             Social-hub
           </h1>
         </div>
-    <div className="flex flex-col items-center space-y-4">
+        <div className="flex flex-col items-center space-y-4">
+          <div className="text-center w-full text-lg font-medium">
+            {`Welcome, @${params.username}`}
+          </div>
 
-    <div className="text-center w-full text-lg font-medium">
-        {`Welcome, @${params.username}`}    
-    </div>
+          <div>
+            <Image
+              src="https://res.cloudinary.com/dsgi2zbq2/image/upload/profile_pic_q6ssck.jpg"
+              alt="profile pic"
+              width={150}
+              height={150}
+              className="rounded-[50%]"
+            />
+          </div>
+          <div className="grid w-full max-w-sm items-center gap-1.5 text-md">
+            <Label htmlFor="picture" className="text-md">
+              Please upload avatar
+            </Label>
 
-    <div>
-        <Image src="https://res.cloudinary.com/dsgi2zbq2/image/upload/profile_pic_q6ssck.jpg" alt="profile pic" width={150} height={150} className='rounded-[50%]'/>
-    
-    </div>
-    <div className="grid w-full max-w-sm items-center gap-1.5 text-md">
-    
-      <Label htmlFor="picture" className='text-md'>Please upload avatar</Label>
-      <Input id="picture" type="file" className='text-md' onChange={onSelectFile}/>
-
-    </div>
-    {
-        imgSrc &&
-
-        <div className="">
-            
             <AlertDialog>
-            <AlertDialogTrigger>
-                Open
-            </AlertDialogTrigger>
+              <AlertDialogTrigger>
+                <Input
+                  id="picture"
+                  type="file"
+                  className="text-md"
+                  onChange={onSelectFile}
+                />
+              </AlertDialogTrigger>
+              {imgSrc && (
                 <AlertDialogContent>
-                    <AlertDialogHeader>
-                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                    <AlertDialogDescription>
-                        <ReactCrop
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>
+                      Selected avatar
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>Move circle to select area.</AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <div>
+                      <ReactCrop
                         crop={crop}
                         circularCrop
                         keepSelection
                         aspect={ASPECT_RETIO}
                         minWidth={MIN_DIMENTION}
                         onChange={(crop, percentCrop) => setCrop(crop)}
-                        >
-                    <Image src={imgSrc} alt='Upload' width={500} height={500} style={{maxHeight:"70vh"}}
-                    onLoad={onImageLoad}
-                    ></Image>
-                    </ReactCrop>
-                    
-
-                    </AlertDialogDescription>
-                    </AlertDialogHeader>
-                    <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction>Continue</AlertDialogAction>
-                    </AlertDialogFooter>
+                        locked
+                      >
+                        <div className=" max-h-[500px] max-w-[500px] flex items-center justify-center overflow-hidden">
+                          <img
+                            src={imgSrc}
+                            alt="Upload"
+                            width={(e) => e.currentTarget.naturalWidth}
+                            height={(e) => e.currentTarget.naturalHeight}
+                            onLoad={onImageLoad}
+                          />
+                        </div>
+                      </ReactCrop>
+                  </div>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel onClick={cancelHandler}>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={continueHandler}>Continue</AlertDialogAction>
+                  </AlertDialogFooter>
                 </AlertDialogContent>
+              )}
             </AlertDialog>
-        
+          </div>
         </div>
-      }
+      </div>
     </div>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
