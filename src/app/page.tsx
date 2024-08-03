6b7bddf79f58e0a66587914ea3785a7171bbad14
Tushar Pawar <tushharpawar14@gmail.com>
2024-08-03 22:42:31 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+'use client'
 import { HeroHighlight } from "@/components/ui/hero-highlight";
 import { HomePage } from "./aceternity/HomePage";
+import { getSession } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
+    
   return (
+    <>
     <main className=" w-full min-h-screen">
-      <HomePage></HomePage>
+      
+         <HomePage></HomePage>
+      
     </main>
+    </>
   );
 }
