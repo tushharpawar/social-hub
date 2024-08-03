2244cc8d9083b4eaaa5e7411c137a5828047b65c
Tushar Pawar <tushharpawar14@gmail.com>
2024-08-03 22:42:45 +0530

    Changed styling

--- src/components/Slidebar.tsx
+++ src/components/Slidebar.tsx
@@ -6,25 +6,25 @@ import { AiOutlinePlusSquare } from "react-icons/ai";
 import { FaRegHeart } from "react-icons/fa";
 import { CgNotes } from "react-icons/cg";
 import { BiMessageRoundedDots } from "react-icons/bi";
-import { MdOutlineNotificationsActive } from "react-icons/md";
 import { FaRegBookmark } from "react-icons/fa";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { MdOutlineSettings } from "react-icons/md";
+import React from "react";
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function Sidebar({ className }: SidebarProps) {
   return (
     <div className={cn(className)}>
-      <div className="space-y-4 py-4 min-h-screen">
+      <div className="w-[20%] text-md fixed border-r-2 border-gray-400 space-y-4 py-4 min-h-screen">
         <div className="px-6 py-2 ">
-          <h1 className="mb-2 px-4 text-2xl font-semibold tracking-tight">
+          <h1 className="px-4 text-2xl font-semibold tracking-tight">
             Social-Hub
           </h1>
         </div>
 
         <div className="px-8 py-2">
-          <div className="space-y-4 ">
+          <div className="space-y-3 ">
             <Button variant="ghost" className="w-full justify-start text-lg">
               <AiOutlineHome className="mr-3 h-6 w-6" />
               Home
@@ -67,7 +67,7 @@ export default function Sidebar({ className }: SidebarProps) {
           </div>
         </div>
 
-        <div className="mb-2 flex items-center px-12 w-full justify-start text-lg">
+        <div className="flex items-center px-12 w-full justify-start text-lg">
           <div className="text-center">
             <Avatar className="mr-3 h-6 w-6">
               <AvatarImage src="https://github.com/shadcn.png" />
@@ -75,7 +75,7 @@ export default function Sidebar({ className }: SidebarProps) {
             </Avatar>
           </div>
 
-          <div className="text-lg font-semibold">Profile</div>
+          <div className="text-lg">Profile</div>
         </div>
       </div>
     </div>
