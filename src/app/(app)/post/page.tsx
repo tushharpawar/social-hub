671592007dc19049369f20cbd238818294d6d6eb
Tushar Pawar <tushharpawar14@gmail.com>
2024-08-12 23:31:39 +0530

    Added like counts and caption

--- src/app/(app)/post/page.tsx
+++ src/app/(app)/post/page.tsx
@@ -13,12 +13,15 @@ import { FaRegHeart } from "react-icons/fa";
 import { FaRegComment } from "react-icons/fa";
 import { IoPaperPlaneOutline } from "react-icons/io5";
 import { FaRegBookmark } from "react-icons/fa";
+import { Separator } from "@/components/ui/separator"
 
-const page = () => {
+
+const Page = () => {
+
   return (
-    <div className="w-full max-w-md min-h-screen ">
-      <div className="h-auto flex justify-center items-center mt-10">
-        <div className="flex items-center justify-center px-5 py-5">
+    <div className="w-full max-w-md max-h-screen ">
+      <div className="h-auto flex justify-center items-center">
+        <div className="flex items-center justify-center px-5 py-3">
           <div className="max-w-[300px] w-full flex items-center gap-3 flex-col ">
             <div className="flex items-center gap-10">
               <div className="flex items-center space-x-4">
@@ -44,7 +47,7 @@ const page = () => {
 
           {/* Post-bottom component */}
 
-            <div className="flex items-center justify-between w-full  px-2">
+            <div className="flex items-center justify-between w-full px-2">
               <div className="flex items-center gap-5">
               <FaRegHeart className="h-5 w-5 cursor-pointer"/>
               <FaRegComment className="h-5 w-5 cursor-pointer"/>
@@ -55,6 +58,16 @@ const page = () => {
               </div>
             </div>
 
+            <div className="flex w-full justify-start items-center px-2">
+               <p className=" font-semibold text-sm">1200 likes</p>
+            </div>
+
+            <div className="flex w-full justify-start items-center px-2">
+            <p className="text-sm font-medium">Hello caption here</p>
+            </div>
+
+            <Separator />
+
           </div>
         </div>
       </div>
@@ -62,4 +75,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
