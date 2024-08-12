a2fd27a742f5f51d458611bcdb5d2889f78620ce
Tushar Pawar <tushharpawar14@gmail.com>
2024-08-12 22:47:44 +0530

    Added post footer component

--- src/app/(app)/post/page.tsx
+++ src/app/(app)/post/page.tsx
@@ -1,17 +1,18 @@
 "use client"
 
 import React from "react";
-import { Image } from "@nextui-org/image";
 import { Skeleton } from "@/components/ui/skeleton";
 import { MdOutlineMoreVert } from "react-icons/md";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { FaRegHeart } from "react-icons/fa";
+import { FaRegComment } from "react-icons/fa";
+import { IoPaperPlaneOutline } from "react-icons/io5";
+import { FaRegBookmark } from "react-icons/fa";
 
 const page = () => {
   return (
@@ -28,18 +29,31 @@ const page = () => {
               </div>
               <div className="">
               <DropdownMenu>
-                <DropdownMenuTrigger ><MdOutlineMoreVert /></DropdownMenuTrigger>
+                <DropdownMenuTrigger ><MdOutlineMoreVert className="h-5 w-5"/></DropdownMenuTrigger>
                 <DropdownMenuContent>
                   <DropdownMenuItem>Profile</DropdownMenuItem>
-                  <DropdownMenuItem>Billing</DropdownMenuItem>
-                  <DropdownMenuItem>Team</DropdownMenuItem>
-                  <DropdownMenuItem className="text-red-500 focus:text-red-500">Subscription</DropdownMenuItem>
+                  <DropdownMenuItem>Share</DropdownMenuItem>
+                  <DropdownMenuItem>Save</DropdownMenuItem>
+                  <DropdownMenuItem className="text-red-500 focus:text-red-500">Delete</DropdownMenuItem>
                 </DropdownMenuContent>
               </DropdownMenu>
               </div>
             </div>
 
             <Skeleton className="h-[300px] w-[300px] space-y-5" />
+
+          {/* Post-bottom component */}
+
+            <div className="flex items-center justify-between w-full  px-2">
+              <div className="flex items-center gap-5">
+              <FaRegHeart className="h-5 w-5 cursor-pointer"/>
+              <FaRegComment className="h-5 w-5 cursor-pointer"/>
+              <IoPaperPlaneOutline className="h-5 w-5 cursor-pointer"/>
+              </div>
+              <div className="">
+              <FaRegBookmark className="h-5 w-5 cursor-pointer"/>
+              </div>
+            </div>
 
           </div>
         </div>
