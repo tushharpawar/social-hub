3b2cb9c85e2b54418d39d0f935bc8341dfe08e71
Tushar Pawar <tushharpawar14@gmail.com>
2024-08-13 22:42:28 +0530

    Updated authOption route

--- src/app/api/v1/posts/[post_id]/unlike/route.ts
+++ src/app/api/v1/posts/[post_id]/unlike/route.ts
@@ -2,7 +2,7 @@ import dbConnect from "@/lib/dbConnect";
 import LikeModel from "@/models/Like.model";
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../../auth/[...nextauth]/options";
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 
 export async function DELETE(req:NextRequest,{params}:{params:{post_id:string}},res:NextResponse) {
     await dbConnect();
