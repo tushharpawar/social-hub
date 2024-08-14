e20795dd57b931e8174fcfbd2c3d37e5049acf15
Tushar Pawar <tushharpawar14@gmail.com>
2024-08-14 23:52:06 +0530

    Added fetchposts

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
+"use client"
 import Slidebar from '@/components/Slidebar'
 import HomeNavbar from '@/components/HomeNavbar'
+import React, { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "@/components/ui/use-toast";
+import { avatar } from '@nextui-org/react';
+import PostPage from '../post/page';
 
-import Page from '../post/page'
 
-const page = () => {
+const Page = () => {
+
+  const [isLoading,setIsLoading] = useState(false)
+  const [posts,setPosts] = useState([])
+
+ const fetchPost = useCallback(
+  async()=>{
+    setIsLoading(true)
+    try {
+      const response = await axios.get('/api/v1/all-posts')
+      
+      // setUsername(response.data.message.post.username)
+      // setAvatar(response.data.message.post.avatar)
+      // setPostSrc(response.data.message.postUrl)
+      // setCaption(response.data.message.caption)
+      setPosts(response.data.message)
+      console.log(posts);
+      console.log(response.data.message);
+      
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error); 
+    }
+  },[])
+
+  useEffect(() => {
+    
+    fetchPost()
+  }, [])
+  
+
   return (
     <main className='flex overflow-hidden'>
       <div className='w-[40vw] min-h-screen'>
@@ -13,11 +47,26 @@ const page = () => {
 
       <div className='w-full min-h-screen'>
         <HomeNavbar></HomeNavbar>
-        <Page/>
+        <div className="mt-2">
+          {
+            posts.length > 0 ?
+            (
+              posts.map((post,index)=>(
+                <PostPage
+                key={index}
+                username={post.owner.username}
+                postUrl={post.postUrl}
+                avatar={post.owner.avatar.url}
+                caption={post.caption}
+                ></PostPage>
+              ))
+            ):(<p>No posts to display</p>)
+          }
+        </div>
     </div>
 
     </main>
   )
 }
 
-export default page
+export default Page
