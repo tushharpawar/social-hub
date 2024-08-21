e9867ee1d9788759d1588d54a7b4867d88299287
Tushar Pawar <tushharpawar14@gmail.com>
2024-08-21 23:43:19 +0530

    Added main home page after login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,19 @@ import { HomePage } from "./aceternity/HomePage";
 import { getSession } from "next-auth/react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import HomePageAfterLogin from "@/components/home/Home";
+import RightSlidebarHeader from "@/components/right-slidebar/RightSlidebarHeader";
+
+
+
 
 
 export default function Home() {
-    
+    const session = useSession()
   return (
     <>
-    <main className=" w-full min-h-screen">
-      
-         <HomePage></HomePage>
-      
+    <main className=" w-full min-h-screen flex items-center justify-center">
+    <HomePageAfterLogin></HomePageAfterLogin>
     </main>
     </>
   );
