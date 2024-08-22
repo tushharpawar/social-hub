67399dd4a126cdea9795c1fefe6165bf983d4b3f
Tushar Pawar <tushharpawar14@gmail.com>
2024-08-22 22:44:07 +0530

    Changed size of characters

--- src/app/schemas/noteSchema.ts
+++ src/app/schemas/noteSchema.ts
@@ -1,5 +1,5 @@
 import {z} from "zod"
 
 export const noteSchema= z.object({
-        content:z.string().min(1,"Note cant'be empty").max(300,"Only 300 characters allowed")
+        content:z.string().min(1,"Note cant'be empty").max(500,"Only 300 characters allowed")
 })
