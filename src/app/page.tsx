9f6dd87d1bf46d30496b9808679406560c6ec37d
Tushar Pawar <tushharpawar14@gmail.com>
2024-08-24 23:23:49 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,13 @@ import { getSession } from "next-auth/react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import HomePageAfterLogin from "@/components/home/Home";
-import RightSlidebarHeader from "@/components/right-slidebar/RightSlidebarHeader";
+import RightSlidebarHeader from "@/components/right-slidebar/RightSlidebar";
 
 
+//Notes::::
+// update File upload component from acertanity ui in create post
+//Add button for like which creates animation on when liked from magic ui (Component name:Cool button)
+//Add animated follow button from magic ui (Component name:Animated subscribe button)
 
 
 
