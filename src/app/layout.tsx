b47200ff0fd648b0f7b11eee33a0ec91f7f0b595
Tushar Pawar <tushharpawar14@gmail.com>
2024-10-28 23:45:52 +0530

    Added provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,12 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from '../context/AuthProvider'
 import { Toaster } from "@/components/ui/toaster"
-import { Providers } from "./providers";
 import Slidebar from '@/components/Slidebar'
+import { store } from '@/app/redux/store'
+import { Provider } from 'react-redux'
 import { useRouter } from 'next/router';
 import RightSlidebarHeader from "@/components/right-slidebar/RightSlidebar";
+import { StoreProvider } from "./redux/StoreProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,15 +28,17 @@ export default function RootLayout({
     <html lang="en">
       <AuthProvider>
       <body className={inter.className}>
-        <Providers>
+
         <main className="flex">
         <div className='w-[25%]'>
         <Slidebar></Slidebar>
         </div>
+          <StoreProvider>
           {children}
+          </StoreProvider>
           </main>
         <Toaster />
-        </Providers>
+
       </body>
       </AuthProvider>
     </html>
