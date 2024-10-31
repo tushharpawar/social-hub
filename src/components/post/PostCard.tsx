68a825b184fcbe068bddb3ee53ae28672c2ad6c8
Tushar Pawar <tushharpawar14@gmail.com>
2024-10-31 21:02:12 +0530

    Added like functionality

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import { MdOutlineMoreVert } from "react-icons/md";
 import {
@@ -18,6 +18,8 @@ import axios from "axios";
 import { Toast } from "../ui/toast";
 import CommentBox from "./CommentBox";
 import { useToast } from "../ui/use-toast";
+import { ConfettiButton } from "../magicui/confetti";
+import { useSelector } from "react-redux";
 
 
 type PostCardProps = {
@@ -26,55 +28,66 @@ type PostCardProps = {
   postUrl:string;
   caption:string;
   postId:any;
+  likeCount:number;
 };
+ 
 
-// like logic
-
-//fetch the like status of user while fetching posts
-//await axios.get('users likes on post')
-//get user status is he has liked the post or not
-//if (!liked){
-//   axios.post('do like route')
-// }
-//else{
-// axios.post('dislike route')
-// }
-
-//on like route get user id from session and post id 
-
-
-
-const PostPage = ({username,avatar,postUrl,caption,postId}:PostCardProps) => {
-
+const PostPage = ({username,avatar,postUrl,caption,postId,likeCount}:PostCardProps) => {
   const [isLiked,setIsLiked] = useState(false)
+  const [likeData,setLikeData] = useState([])
+  const [newLikeCount,setNewLikeCount] = useState(likeCount)
   const [isCommentClicked,setIsCommentClicked] = useState(false)
   const {toast} = useToast()
 
+  //increase & decrease like and update state of isLiked on fronted
+  //the changes will be appear first on ui and later it will updated in db
+
   const onLike =async ()=>{
       try {
+        if(!isLiked ){
+          setNewLikeCount(newLikeCount+1)
+        }else{setNewLikeCount(newLikeCount-1)}
+        setIsLiked(!isLiked)
+
         const response = await axios.post(`/api/v1/posts/${postId}/like`)
   
         if(response.status === 201){
           toast({
             title:'Liked'
           })
-
-          setIsLiked(!isLiked)
         }
       } catch (error) {
         console.log(error);
         toast({
           title:'Not Liked'
         })
-      }
+     }
 }
 
+// checks if logged-in user liked post or not if user not liked then isLiked state will be false and if it is liked then isLiked state will be true
+
+const getLikedByLoggedInUser = async()=>{
+    try {
+    const response = await axios.get(`api/v1/check-logged-in-user-liked/${postId}`)
+    if(response.data.message === true){
+      setIsLiked(true)
+    }
+    if(response.data.message === false){
+      setIsLiked(false)
+    }
+    } catch (error) {
+      console.log("Error while fething likes",error);
+    }
+}
+
+useEffect(()=>{
+  getLikedByLoggedInUser()
+},[])
+
+//opens comment box
 const onComment =async () =>{
-
   setIsCommentClicked(!isCommentClicked)
-
 }
- 
 
   return (
     <div className="w-full h-auto flex justify-center mt-5">
@@ -105,8 +118,8 @@ const onComment =async () =>{
                   <Skeleton className="h-3 w-[170px]" />
                 )
               }
-
               </div>
+
               <div className="">
               <DropdownMenu>
                 <DropdownMenuTrigger ><MdOutlineMoreVert className="h-5 w-5"/></DropdownMenuTrigger>
@@ -137,6 +150,9 @@ const onComment =async () =>{
 
             <div className="flex items-center justify-between w-full px-2">
               <div className="flex items-center gap-5">
+
+            {/* conditional rendering based on isliked state */}
+
               {
                 isLiked ? <FaHeart size={22} className="cursor-pointer text-red-600" onClick={onLike}/> : 
                 <FaRegHeart size={22} className=" cursor-pointer" onClick={onLike}/>
@@ -151,7 +167,7 @@ const onComment =async () =>{
             </div>
 
             <div className="flex w-full justify-start items-center px-2">
-               <p className=" font-semibold text-sm">1200 likes</p>
+               <p className=" font-semibold text-sm">{newLikeCount} likes</p>
             </div>
 
             <div className="flex w-full justify-start items-center px-2">
@@ -163,6 +179,7 @@ const onComment =async () =>{
             {              
               isCommentClicked ? <CommentBox postId={postId} isCommentClicked={isCommentClicked}></CommentBox> :<></>
             }
+            
              <Separator />
           </div>
         </div>
