edbd040ce7377f1e683917017c022d20b3df7887
Tushar Pawar <tushharpawar14@gmail.com>
2024-11-04 23:48:59 +0530

    added comment slice

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,28 +1,12 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import commentSlice  from './commentSlice'
 import authSlice from './authSlice'
 import postSlice from './postSlice'
-
-// import {
-//   persistReducer,
-// } from 'redux-persist'
-// import storage from 'redux-persist/lib/storage'
-
-// const persistConfig = {
-//   key: 'root',
-//   version: 1,
-//   storage
-// }
-
-// const rootReducer = combineReducers({
-//   auth:authSlice
-// // })
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
   reducer:{
     auth:authSlice,
     post:postSlice,
+    comment:commentSlice,
   }
 })
