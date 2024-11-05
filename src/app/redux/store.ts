3929a985020ca54b571bf196280517b936404333
Tushar Pawar <tushharpawar14@gmail.com>
2024-11-05 23:24:13 +0530

    Added userProfile

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import commentSlice  from './commentSlice'
 import authSlice from './authSlice'
 import postSlice from './postSlice'
+import userProfileSlice from './userProfileSlice'
 
 export const store = configureStore({
   reducer:{
     auth:authSlice,
     post:postSlice,
     comment:commentSlice,
+    userProfile:userProfileSlice,
   }
 })
