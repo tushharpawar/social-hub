da45b09b80ef84d74b29f5ea93a3e4c87a8a5dbc
Tushar Pawar <tushharpawar14@gmail.com>
2024-11-07 23:46:12 +0530

    Update Slidebar.tsx

--- src/components/Slidebar.tsx
+++ src/components/Slidebar.tsx
@@ -1,90 +1,78 @@
+'use client'
+
 import { cn } from "@/lib/utils";
 import { AiOutlineHome } from "react-icons/ai";
 import { IoSearchOutline } from "react-icons/io5";
-import { AiOutlinePlusSquare } from "react-icons/ai";
 import { FaRegHeart } from "react-icons/fa";
 import { CgNotes } from "react-icons/cg";
 import { BiMessageRoundedDots } from "react-icons/bi";
-import { FaRegBookmark } from "react-icons/fa";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { MdOutlineSettings } from "react-icons/md";
 import React from "react";
 import Link from "next/link";
-import CreatePostAlert from '../components/create-post/CreatePostAlert'
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
+import CreatePostAlert from "../components/create-post/CreatePostAlert";
 import { Button } from "./ui/button";
-import { Input } from "./ui/input";
-import { useRouter } from "next/navigation";
+import { useSelector } from "react-redux";
+import { useSession } from "next-auth/react";
+import { User } from "next-auth";
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function Sidebar({ className }: SidebarProps) {
-
-
+  const {data:session} = useSession()
+  const user:User = session?.user as User
+  
   return (
     <div className={cn(className)}>
       <div className="flex justify-start">
-      <div className="w-[20%] text-md fixed border-r-2 border-gray-400 space-y-4 py-4 min-h-screen">
-        <div className="px-6 py-2 ">
-          <h1 className="px-4 text-2xl font-semibold tracking-tight">
-            Social-Hub
-          </h1>
-        </div>
+        <div className="w-[20%] text-md fixed border-gray-600 border-r space-y-4 py-4 min-h-screen z-50">
+          <div className="px-6 py-2">
+            <h1 className="px-4 text-2xl font-semibold tracking-tight">
+              Social-Hub
+            </h1>
+          </div>
 
-        <div className="px-8 py-2">
-          <div className="space-y-3 ">
-            <Button variant="ghost" className="w-full justify-start text-lg">
-              <Link href={'/'} className="flex">
-              <AiOutlineHome className="mr-3 h-6 w-6" />
-              Home
-              </Link>
-            </Button>
+          <div className="px-8 py-2">
+            <div className="space-y-3 ">
+              <Button variant="ghost" className="w-full justify-start text-lg">
+                <Link href={"/"} className="flex">
+                  <AiOutlineHome className="mr-3 h-6 w-6" />
+                  Home
+                </Link>
+              </Button>
 
-            <Button variant="ghost" className="w-full justify-start text-lg">
-              <IoSearchOutline className="mr-3 h-6 w-6" fill="true" />
-              Stories
-            </Button>
+              <Button variant="ghost" className="w-full justify-start text-lg">
+                <IoSearchOutline className="mr-3 h-6 w-6" fill="true" />
+                Stories
+              </Button>
 
-          <CreatePostAlert/>
-            
-            <Button variant="ghost" className="w-full justify-start text-lg">
-            <Link href={'/note'} className="flex">
-            <CgNotes className="mr-3 h-6 w-6" />
-            Notes
-            </Link>
-              
-            </Button>
+              <CreatePostAlert />
 
+              <Button variant="ghost" className="w-full justify-start text-lg">
+                <Link href={"/note"} className="flex">
+                  <CgNotes className="mr-3 h-6 w-6" />
+                  Notes
+                </Link>
+              </Button>
 
-            <Button variant="ghost" className="w-full justify-start text-lg">
-              <BiMessageRoundedDots className="mr-3 h-6 w-6" />
-              Messages
-            </Button>
+              <Button variant="ghost" className="w-full justify-start text-lg">
+                <Link href={`/inbox/${user?._id}`} className="flex">
+                  <BiMessageRoundedDots className="mr-3 h-6 w-6" />
+                  Messages
+                </Link>
+              </Button>
 
-            <Button variant="ghost" className="w-full justify-start text-lg">
-              <FaRegHeart className="mr-3 h-6 w-6" />
-              Notification
-            </Button>
+              <Button variant="ghost" className="w-full justify-start text-lg">
+                <FaRegHeart className="mr-3 h-6 w-6" />
+                Notification
+              </Button>
 
-            <Button variant="ghost" className="w-full justify-start text-lg">
-              <MdOutlineSettings className="mr-3 h-6 w-6" />
-              Settings
-            </Button>
+              <Button variant="ghost" className="w-full justify-start text-lg">
+                <MdOutlineSettings className="mr-3 h-6 w-6" />
+                Settings
+              </Button>
+            </div>
           </div>
         </div>
-
-
-      </div>
       </div>
     </div>
   );
