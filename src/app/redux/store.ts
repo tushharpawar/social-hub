8d772134ac1f6f958789c9ec3df4e321084698a7
Tushar Pawar <tushharpawar14@gmail.com>
2024-11-07 23:44:21 +0530

    Update store.ts

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -3,6 +3,7 @@ import commentSlice  from './commentSlice'
 import authSlice from './authSlice'
 import postSlice from './postSlice'
 import userProfileSlice from './userProfileSlice'
+import updateAvatarSlice from './updateAvatarSlice'
 
 export const store = configureStore({
   reducer:{
@@ -10,5 +11,6 @@ export const store = configureStore({
     post:postSlice,
     comment:commentSlice,
     userProfile:userProfileSlice,
+    isClickedOnEditProfile:updateAvatarSlice,
   }
 })
