207243f11f0893116d6fdffcf8bd822244bbb8db
Tushar Pawar <tushharpawar14@gmail.com>
2024-11-07 23:44:17 +0530

    Update postSlice.ts

--- src/app/redux/postSlice.ts
+++ src/app/redux/postSlice.ts
@@ -3,14 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 const postSlice = createSlice({
     name:"post",
     initialState:{
-        posts:null
+        posts:null,
+        fetchedUserPosts:[],
     },
     reducers:{
         setPosts:(state,action)=>{
             state.posts = action.payload
         },
+        setFetchedUserPosts:(state,action)=>{
+            state.fetchedUserPosts = action.payload
+        }
     }
 })
 
-export const {setPosts} = postSlice.actions
+export const {setPosts,setFetchedUserPosts} = postSlice.actions
 export default postSlice.reducer
