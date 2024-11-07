9f1860a806379bf7021144b9dd263da422590663
Tushar Pawar <tushharpawar14@gmail.com>
2024-11-07 23:44:11 +0530

    Update authSlice.ts

--- src/app/redux/authSlice.ts
+++ src/app/redux/authSlice.ts
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const authSlice = createSlice({
     name:'auth',
     initialState:{
-        user:null
+        user:[]
     },
     reducers:{
         setAuthUser:(state,action)=>{
