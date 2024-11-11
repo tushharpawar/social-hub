4754a4e50ad792e3aeb973d0dbe3d09316b71437
Tushar Pawar <tushharpawar14@gmail.com>
2024-11-11 23:51:58 +0530

    Update DateSpaerator.tsx

--- src/components/inbox/DateSpaerator.tsx
+++ src/components/inbox/DateSpaerator.tsx
@@ -11,7 +11,7 @@ const DateSpaerator = (
     }
   return (
     <div className='relative flex items-center justify-center my-6'>
-        <span className='absolute left-auto right-auto text-xs font-semibold text-gray-600 bg-white px-2'>
+        <span className='absolute left-auto right-auto text-xs font-semibold text-gray-600 px-2'>
             {formatDate(date)}
         </span>
     </div>
