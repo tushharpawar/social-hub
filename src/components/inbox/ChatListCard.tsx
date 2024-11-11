d7f3b4b79ff0d73d7a3d536340afe995fc3da259
Tushar Pawar <tushharpawar14@gmail.com>
2024-11-11 23:52:12 +0530

    Update ChatListCard.tsx

--- src/components/inbox/ChatListCard.tsx
+++ src/components/inbox/ChatListCard.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Avatar, AvatarImage } from '../ui/avatar'
+import { useChatContext, useMessageListContext } from 'stream-chat-react';
 
 const ChatListCard = (props) => {
   // const { channel, setActiveChannel } = props;
-  const { channel, displayImage, displayTitle, latestMessagePreview } = props;
+  const {setActiveChannel} = useChatContext()
+  const { channel } = props;
+  const [activeChannelId, setActiveChannelId] = useState(false)
+  
+
+  const activeChannel = () =>{
+    setActiveChannelId(true)
+    setActiveChannel(channel)  }
+
   return (
-    <div className="w-[95%] flex bg-gray-100 rounded-md border-b justify-center m-1 p-2 cursor-pointer">
+    <div className={`w-[95%] flex transition duration-200 transform hover:scale-105 active:scale-95 active:bg-gray-300 border-b justify-center m-1 p-2 cursor-pointer`} onClick={activeChannel}>
             <div>
             <Avatar className="mr-3 h-10 w-10">
               <AvatarImage src="https://res.cloudinary.com/tushharpawar/image/upload/v1725987768/avatar/hrcubc6bbowfwelcjheh.jpg"/>
@@ -14,7 +23,7 @@ const ChatListCard = (props) => {
 
             <div className="w-full">
                 <div className="text-sm">{channel.id}</div>
-                <div className="text-sm text-gray-500">{latestMessagePreview}</div>
+                <div className="text-sm text-gray-500">{activeChannelId}</div>
             </div>
             </div>
 
