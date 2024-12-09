4ed00f0a3a749f44a5c1bc09ec4b539f7f1159f3
Tushar Pawar <tushharpawar14@gmail.com>
2024-12-09 22:44:56 +0530

    Update StoreProvider.tsx

--- src/app/redux/StoreProvider.tsx
+++ src/app/redux/StoreProvider.tsx
@@ -3,13 +3,15 @@
 import { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { store } from "./store";
-
-
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor } from "./store";
 
 export const StoreProvider = ({children}:{children:ReactNode}) =>{
     return (
         <Provider store={store}>
-            {children}
-        </Provider>
+        <PersistGate loading={null} persistor={persistor}>
+          {children}
+        </PersistGate>
+      </Provider>
     )
 }
