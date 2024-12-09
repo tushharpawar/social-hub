cf856348f270e57586f549e66dcc16abb830f20b
Tushar Pawar <tushharpawar14@gmail.com>
2024-12-09 22:45:10 +0530

    Added persist store

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,16 +1,45 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import commentSlice  from './commentSlice'
 import authSlice from './authSlice'
 import postSlice from './postSlice'
 import userProfileSlice from './userProfileSlice'
 import updateAvatarSlice from './updateAvatarSlice'
 
-export const store = configureStore({
-  reducer:{
-    auth:authSlice,
-    post:postSlice,
-    comment:commentSlice,
-    userProfile:userProfileSlice,
-    isClickedOnEditProfile:updateAvatarSlice,
-  }
+const combinedReducers = combineReducers({
+      auth:authSlice,
+      post:postSlice,
+      comment:commentSlice,
+      userProfile:userProfileSlice,
+      isClickedOnEditProfile:updateAvatarSlice,
 })
+
+const persistedReducer = persistReducer(
+  {
+      key: 'root',
+      storage,
+  },
+  combinedReducers
+)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+          serializableCheck: {
+              ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+      }),
+})
+
+export const persistor = persistStore(store)
