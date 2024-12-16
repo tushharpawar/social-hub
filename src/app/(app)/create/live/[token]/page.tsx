2b2e4e22693e5e1c7f37b47b66cbb2453a3a12f0
Tushar Pawar <tushharpawar14@gmail.com>
2024-12-16 23:00:50 +0530

    Update page.tsx

--- src/app/(app)/create/live/[token]/page.tsx
+++ src/app/(app)/create/live/[token]/page.tsx
@@ -9,19 +9,23 @@ import { useParams, useRouter } from "next/navigation";
 import {
   StreamVideoClient,
   LivestreamPlayer,
-    StreamCall,
-    StreamVideo,
-    useCallStateHooks,
-    CallControls,
-    SpeakerLayout,
-    User,
-    StreamTheme,
- } from "@stream-io/video-react-sdk";
-import { v4 as uuid } from "uuid";
+  StreamCall,
+  StreamVideo,
+  useCallStateHooks,
+  CallControls,
+  SpeakerLayout,
+  User,
+  StreamTheme,
+  Call,
+} from "@stream-io/video-react-sdk";
+import cuid from "cuid";
 import { ImPhoneHangUp } from "react-icons/im";
+import { IoVideocamOutline } from "react-icons/io5";
+import { IoVideocamOffOutline } from "react-icons/io5";
+import { Loader2 } from "lucide-react";
 
 const apiKey = "536ez6cv3czw";
-const callId = uuid();
+const callId = cuid();
 
 const Page = () => {
   const { data: session } = useSession();
@@ -29,16 +33,31 @@ const Page = () => {
   const [client, setClient] = useState<StreamVideoClient | undefined>(
     undefined
   );
+  const [call, setCall] = useState<Call | undefined>(undefined);
   const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter()
+  const router = useRouter();
+
   useEffect(() => {
-    if (session && session.user) {
+    const connectUser = async () => {
       setIsLoading(true);
-      connectUser();
-      call?.getOrCreate();
+      const client = new StreamVideoClient({
+        apiKey,
+        user,
+        token: token as string,
+      });
+      setClient(client);
+
+      const call = client?.call("livestream", callId);
+      setCall(call);
+
+      call?.camera.enable();
+      call?.microphone.enable();
+      await call?.getOrCreate();
       setIsLoading(false);
-    }
-  }, [session,token]);
+    };
+
+    connectUser();
+  }, [session, token]);
 
   if (!session || !session.user) {
     return <div>No session</div>;
@@ -46,57 +65,94 @@ const Page = () => {
   const authUser = session.user as authUser;
 
   const user: User = {
-    id: authUser?.username|| "",
+    id: authUser?.username || "",
     name: authUser.username || "",
     image: authUser?.avatar || "",
   };
-  const call = client?.call("livestream", callId);
 
-  async function connectUser() {
-    const client = new StreamVideoClient({
-      apiKey,
-      user,
-      token: token as string,
-    });
-    setClient(client);
-    await client.connectUser({ id: authUser?.username || "" }, token);
+  if (!call || !client) {
+    return <div style={{maxHeight:"screen"}}> Something went wrong please try again later!</div>;
   }
 
-  const handleStopLive = () =>{
-    call?.endCall()
-    router.replace('/live')
+  if(isLoading){
+    return (
+      <div style={{height:"full"}}>  
+      <Loader2 size={40}/> 
+      <p style={{fontSize:"40px", textAlign:"center"}}>Initiating Live ...</p>
+      </div>
+    );
   }
+
+  const handleStopLive = () => {
+    call?.endCall();
+    router.replace("/live");
+  };
+
+  const handleDisableVideo = () => {
+    call?.camera.disable();
+  };
+  const handleDisableMicrophone = () => {
+    call?.microphone.disable();
+  };
+
+  const handleEnableVideo = () => {
+    call?.camera.enable();
+  };
 
   return (
     <div className="h-screen">
-
-      {client && !isLoading ? (
+      {client && call ? (
         <div className="">
           <div className="relative h-screen">
             <StreamVideo client={client}>
               <StreamTheme style={{ fontFamily: "sans-serif", color: "white" }}>
                 <StreamCall call={call}>
-                    <div className="h-screen relative">
-                      <LivestreamPlayer
-                        callType="livestream"
-                        callId={callId}
-                        layoutProps={{
-                          showLiveBadge: true,
-                          showDuration: true,
-                          showParticipantCount: true,
-                        }}
+                  <div className="h-screen relative p-4">
+                    <LivestreamPlayer
+                      callType="livestream"
+                      callId={callId}
+                      layoutProps={{
+                        showLiveBadge: true,
+                        showDuration: true,
+                        showParticipantCount: true,
+                      }}
+                    />
+                    <div className="absolute top-6 right-10">
+                      <ImPhoneHangUp size={24} onClick={handleStopLive} />
+                    </div>
+                    <div className="absolute top-12 right-10">
+                      {/* {call?.camera.enabled ? (
+                        <IoVideocamOutline
+                          size={24}
+                          onClick={handleDisableVideo}
+                        />
+                      ) : (
+                        <IoVideocamOffOutline
+                          size={24}
+                          onClick={handleEnableVideo}
+                        />
+                      )} */}
+
+                        <IoVideocamOutline
+                          size={24}
+                          onClick={handleDisableVideo}
+                        />
+                    </div>
+                    <div className="absolute top-18 right-10">
+                      <ImPhoneHangUp
+                        size={24}
+                        onClick={handleDisableMicrophone}
                       />
-                      <div className="absolute top-6 right-10">
-                      <ImPhoneHangUp size={24} onClick={handleStopLive}/>
                     </div>
-                    </div>
-                    
+                  </div>
                 </StreamCall>
               </StreamTheme>
             </StreamVideo>
           </div>
         </div>
-      ) : "Loading ...."}
+      ) : (
+        "Something went wrong pls try again later!"
+      )}
     </div>
   );
 };
