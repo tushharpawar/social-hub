3baba049b8a45872dae3ef804986d7b58f976566
Tushar Pawar <tushharpawar14@gmail.com>
2024-12-17 23:23:33 +0530

    Update route.ts

--- src/app/api/v1/post-delete/[postId]/route.ts
+++ src/app/api/v1/post-delete/[postId]/route.ts
@@ -4,22 +4,13 @@ import PostModel from "@/models/Post.model";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(req:NextRequest,{params}:{params:{postId:string}},res:NextResponse) {
+export async function POST(req:NextRequest,{params}:{params:{postId:string}},res:NextResponse) {
     
     await dbConnect();
     //Getting userId from logged in user from Session
     const postId = params.postId
-    const session = await getServerSession(authOptions)
-    const _user = session?.user;
 
-    if(!session || !_user){
-        return NextResponse.json(
-            { success: false, message: 'Not authenticated' },
-            { status: 401 }
-          );
-    }
-
-    const userId = _user._id;
+    const userId = await req.json();
 
     try {
 
@@ -30,13 +21,6 @@ export async function DELETE(req:NextRequest,{params}:{params:{postId:string}},r
                 success: false,
                 message: "Post does not exist!",
             }, {status: 401});
-          }
-
-          if(post.owner !== userId){
-                return NextResponse.json({
-                    success: false,
-                    message: "You are not owner of this post!",
-                }, {status: 401});
           }
 
         await post.deleteOne({id:postId});
