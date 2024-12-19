4cd8ebb2a9ab52f405c0902fc5610623882dec44
Tushar Pawar <tushharpawar14@gmail.com>
2024-12-19 23:55:24 +0530

    Update theme-provider.tsx

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,9 +1,23 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useSelector } from "react-redux";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export default function ThemeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { theme } = useSelector((state: any) => state.theme);
+
+  return (
+    <NextThemesProvider
+      attribute="class" 
+      value={{ light:"light", dark:"dark"}}
+      forcedTheme={theme} // Override theme based on Redux state
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
