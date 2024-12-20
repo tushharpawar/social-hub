002a1d617e68f031dcb3b790227e81a785ef380d
Tushar Pawar <tushharpawar14@gmail.com>
2024-12-20 23:55:33 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,23 +7,14 @@ import { Button } from "@/components/ui/button";
 import HomePageAfterLogin from "@/components/home/Home";
 import RightSlidebarHeader from "@/components/right-slidebar/RightSlidebar";
 import Slidebar from '@/components/Slidebar'
+import { useSelector } from "react-redux";
+import HomeNavbar from "@/components/SearchBar";
 
 
 //Notes::::
 //Leaving on 13-11
-//TODO: Add button for like which creates animation on when liked from magic ui (Component name:Cool button)
-//TODO: Add Video call, audio call & live stream
-//TODO: Add story functionality
-//TODO: Add like, comment functionality in notes
-//TODO: Add isSeen model in db and when user visit,like or see the post on feed update it
-//      in db that post should not be fatched again on feed 
-//TODO: Add setting for dark theme and logout button
-//TODO: Add infinite scroll on feed
 //TODO: Add forget password functionality
-
 //FIXME: Make website reponsive
-//FIXME: Optimise post and like functionality
-//FIXME: Optimise react-redux
 //FIXME: Add home page based on if user logged in or not
 //FIXME: Add real email service
 
@@ -31,8 +22,15 @@ export default function Home() {
   
   return (
     <>
-    <main className="w-full min-h-screen flex items-center justify-center">
-      <HomePageAfterLogin></HomePageAfterLogin>
+    <main className={`w-full min-h-screen flex`}>
+        <div className='sm:w-[25%]'>
+          <Slidebar></Slidebar>
+         </div>
+          <div className="absolute top-0 w-full bg-white border-gray-300 dark:bg-black dark:border-zinc-700 flex justify-around py-2 z-50 sm:hidden">
+          <HomeNavbar/>   
+          <RightSlidebarHeader/>     
+        </div>
+        <HomePageAfterLogin></HomePageAfterLogin>
     </main>
     </>
   );
