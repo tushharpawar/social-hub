17e5910a791b97651a24d1762ec85e4d08a52d01
Tushar Pawar <tushharpawar14@gmail.com>
2024-12-21 17:03:15 +0530

    Update CustomChannelHeader.tsx

--- src/components/inbox/CustomChannelHeader.tsx
+++ src/components/inbox/CustomChannelHeader.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React from "react";
-import { useChannelStateContext } from "stream-chat-react";
+import { DefaultStreamChatGenerics, useChannelStateContext } from "stream-chat-react";
 import { Avatar, AvatarImage } from "../ui/avatar";
-import { StreamChat } from "stream-chat";
+import { StreamChat, UserResponse } from "stream-chat";
 import Link from "next/link";
 
 const client = StreamChat.getInstance(process.env.NEXT_PUBLIC_STREAM_API_KEY!);
@@ -15,14 +15,14 @@ const CustomChannelHeader = ({token}) => {
     const otherUser =
     Object.values(channel?.state?.members || {}).find(
       (member) => member.user && member.user.id !== client?.userID
-    )?.user || "";
+    )?.user as UserResponse<DefaultStreamChatGenerics> || null;
 
 
   return (
     <div className="flex items-center justify-between space-x-3 p-3 border-b-2 border-b-gray-200">
       <Link href={`/${otherUser?.id}`} className="flex gap-2 items-center">
         <Avatar className="mr-3 h-12 w-12">
-          <AvatarImage src={otherUser?.image} />
+          <AvatarImage src={otherUser?.image || ""} />
         </Avatar>
 
         <div className="text-lg font-semibold">
