c712509f5b9d207618bf4c716adac2a1b45049ed
Tushar Pawar <tushharpawar14@gmail.com>
2024-12-21 17:03:18 +0530

    Update NoteCard.tsx

--- src/components/notes/NoteCard.tsx
+++ src/components/notes/NoteCard.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {Card, CardBody,} from "@nextui-org/react";
 import { Separator } from "@/components/ui/separator"
 import { MdOutlineMoreVert } from "react-icons/md";
 import {
@@ -8,7 +7,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { User } from "next-auth";
