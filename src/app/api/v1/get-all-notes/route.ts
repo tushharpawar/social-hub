36469a07e7b0f4a4817371beb49deca633019157
Tushar Pawar <tushharpawar14@gmail.com>
2024-12-21 17:31:27 +0530

    Update route.ts

--- src/app/api/v1/get-all-notes/route.ts
+++ src/app/api/v1/get-all-notes/route.ts
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic';
-
 import dbConnect from "@/lib/dbConnect";
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
