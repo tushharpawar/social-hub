2a15018126c5e8212fd0e3fe3fe58b22dc42e97b
Tushar Pawar <tushharpawar14@gmail.com>
2024-12-21 15:35:40 +0530

    Update route.ts

--- src/app/api/v1/sign-up/route.ts
+++ src/app/api/v1/sign-up/route.ts
@@ -49,7 +49,7 @@ export const POST = async (req: Request, res: NextResponse) => {
                 const expiryDate = new Date()
                 expiryDate.setHours(expiryDate.getHours()+1) 
                 
-                let avatar:string="https://res.cloudinary.com/dsgi2zbq2/image/upload/profile_pic_q6ssck.jpg"
+                let avatar:string="https://res.cloudinary.com/tushharpawar/image/upload/v1721905572/profile_pic_q6ssck.jpg"
 
                 const newUser = new UserModel({
                     username,
