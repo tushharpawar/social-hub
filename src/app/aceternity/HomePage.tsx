92c51559067d8a2b9d84097ff4306b3c7443a27b
Tushar Pawar <tushharpawar14@gmail.com>
2024-12-21 15:35:32 +0530

    Update HomePage.tsx

--- src/app/aceternity/HomePage.tsx
+++ src/app/aceternity/HomePage.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { motion } from "framer-motion";
 import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
-import { CarouselPlugin } from "@/app/shadcn/autoCard";
 import Navbar from "../../components/Navbar";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export function HomePage() {
   return (
@@ -27,13 +28,24 @@ export function HomePage() {
       >
         Welcome to <Highlight>Social-Hub</Highlight>.
         <p>Share what you&apos;re into with the people who get you.</p>
+
+        <p className="text-base mt-8  dark:text-gray-300 text-gray-600">
+      Post moments that matter, like, comment, and share your favorites. Connect in real-time with chat and live streams. 
+      </p>
+
+        <p className="text-base mt-4  dark:text-gray-300 text-gray-600">
+        Social-Hub is where your passions find a voice and your connections come to life. Join the community and share your world today.
+        </p>
       </motion.h1>
 
-      <div className="w-full mt-5 flex justify-center items-center">
-
-      {/* image carousel from shadcn */}
-      <CarouselPlugin></CarouselPlugin>
       
+
+      <div className="w-full mt-8 flex justify-center items-center">
+
+      <div className="flex gap-5 font-bold ">
+            <div ><Button  className=" text-md "><Link href={'/sign-in'}>Log in</Link></Button></div>
+            <div ><Button variant="outline" className=" text-md "><Link href={'/sign-up'}>Sign up</Link></Button></div>
+          </div>
       </div>
 
     </HeroHighlight>
