d1718307e35b2c5fd90769437c9c66ff9c0f2ffe
Tushar Pawar <tushharpawar14@gmail.com>
2024-12-21 15:35:55 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,39 @@
 'use client'
-import { HeroHighlight } from "@/components/ui/hero-highlight";
 import { HomePage } from "./aceternity/HomePage";
-import { getSession } from "next-auth/react";
 import { useSession, signIn, signOut } from "next-auth/react";
-import { Button } from "@/components/ui/button";
 import HomePageAfterLogin from "@/components/home/Home";
 import RightSlidebarHeader from "@/components/right-slidebar/RightSlidebar";
 import Slidebar from '@/components/Slidebar'
-import { useSelector } from "react-redux";
 import HomeNavbar from "@/components/SearchBar";
+import CreatePostAlert from "@/components/create-post/CreatePostAlert";
+import { User } from "next-auth";
 
-
-//Notes::::
-//Leaving on 13-11
-//TODO: Add forget password functionality
-//FIXME: Make website reponsive
-//FIXME: Add home page based on if user logged in or not
-//FIXME: Add real email service
 
 export default function Home() {
+
+  const {data:session} = useSession()
+  const user:User = session?.user as User 
   
   return (
     <>
     <main className={`w-full min-h-screen flex`}>
-        <div className='sm:w-[25%]'>
+      {
+        session ? (
+          <>
+          <div className='sm:w-[25%]'>
           <Slidebar></Slidebar>
          </div>
           <div className="absolute top-0 w-full bg-white border-gray-300 dark:bg-black dark:border-zinc-700 flex justify-around py-2 z-50 sm:hidden">
+          <CreatePostAlert/>
           <HomeNavbar/>   
           <RightSlidebarHeader/>     
         </div>
         <HomePageAfterLogin></HomePageAfterLogin>
+          </>
+        ) :(
+          <HomePage/>
+        )
+      }
     </main>
     </>
   );
