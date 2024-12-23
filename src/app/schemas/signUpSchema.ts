e47629994461eb9488d7edfe0b521d56146a7298
Tushar Pawar <tushharpawar14@gmail.com>
2024-12-24 00:41:17 +0530

    Update signUpSchema.ts

--- src/app/schemas/signUpSchema.ts
+++ src/app/schemas/signUpSchema.ts
@@ -4,7 +4,9 @@ export const signUpSchema = z.object({
     username:z.string()
     .min(2,"Usernameshould not be less than 2 characters")
     .max(15,"Username must be less than 15 characters")
-    .regex(/^[a-z][a-z0-9._]+$/,'Username must not contain special characters'),
+    .regex(/^[a-z][a-z0-9._]+$/,'Username must not contain special characters')
+    .toLowerCase()
+    ,
 
     email:z.string().email({message:"Invalid email address"}),
 
